d5c4113cec9e9d1ec5a272658412dd983ee6efb8
neo-level <frederik@neo-level.com>
2022-12-28 08:07:29 +0100

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Frederik Desmet on 28/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
